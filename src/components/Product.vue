e70b944e9e8b9c2faacc9880e18767a1c06ee3b3
David Colacilli <david.colacilli@endava.com>
2020-06-19 18:22:07 -0300

    list products

---
+++ src/components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <img :src="product.image" :alt="product.name" width="200">
+        <h4>${{product.price}}</h4>
+        <h3>{{product.name}}</h3>
+    </section>    
+</template>
+<script>
+export default {
+    name: 'Product',
+    props: {
+        product: Object
+    }
+}
+</script>
+<style scoped>
+section {
+    margin: 1rem; 
+    padding: 1rem; 
+    /* border: 1px solid #eee; */
+    box-shadow: 0 1px 1px rgba(0,0,0,0.2);
+}
+img {margin: -1rem -1rem 0;}
+h4 {
+    margin: 0;
+    font-size: 1.6rem;
+    font-weight: 400;
+}
+h3 {
+    margin: 0;
+    font-size: 1.2rem;
+    font-weight: 400;
+    color: #777;
+}
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,24 @@
+<template>
+    <article>
+        <product v-for="product in products" :key="product.id" :product="product"/>
+    </article>    
+</template>
+<script>
+import products from '../products.json';
+import Product from './Product.vue';
+
+export default {
+    name: 'Products',
+    components: {
+        Product
+    },
+    data() {
+        return {
+            products: products,
+        }
+    }
+}
+</script>
+<style scoped>
+article {display: flex; flex-direction: row; flex-wrap: wrap;}
+</style>
